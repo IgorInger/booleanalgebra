535a3f9ad7fa51fbfbe51f4d45866bd06a63c1b2
Igor Inger <igor.inger@gmail.com>
2012-03-08 21:01:00 +0100

    Add a new view dynamically

--- gui/src/main/java/de/inger/booleanalgebra/gui/commands/NewFileCommand.java
+++ gui/src/main/java/de/inger/booleanalgebra/gui/commands/NewFileCommand.java
@@ -1,6 +1,19 @@
 package de.inger.booleanalgebra.gui.commands;
 
+import org.springframework.beans.MutablePropertyValues;
+import org.springframework.beans.factory.config.AutowireCapableBeanFactory;
+import org.springframework.beans.factory.support.BeanDefinitionRegistry;
+import org.springframework.beans.factory.support.GenericBeanDefinition;
+
+import org.springframework.richclient.application.Application;
+import org.springframework.richclient.application.ViewDescriptor;
+import org.springframework.richclient.application.docking.vldocking.VLDockingViewDescriptor;
+import org.springframework.richclient.application.support.DefaultApplicationServices;
+import org.springframework.richclient.command.ActionCommand;
+import org.springframework.richclient.command.CommandGroup;
 import org.springframework.richclient.command.support.ApplicationWindowAwareCommand;
+
+import de.inger.booleanalgebra.gui.view.InputTextView;
 
 public class NewFileCommand extends ApplicationWindowAwareCommand {
 
@@ -16,7 +29,31 @@ public class NewFileCommand extends ApplicationWindowAwareCommand {
 
     @Override
     protected void doExecuteCommand() {
+	DefaultApplicationServices services = (DefaultApplicationServices) Application.services();
 
+	AutowireCapableBeanFactory factory = services.getApplicationContext()
+		.getAutowireCapableBeanFactory();
+
+	GenericBeanDefinition beanDefinition = new GenericBeanDefinition();
+	beanDefinition.setBeanClass(VLDockingViewDescriptor.class);
+	MutablePropertyValues values = new MutablePropertyValues();
+	values.addPropertyValue("viewClass", InputTextView.class.getName());
+	beanDefinition.setPropertyValues(values);
+
+	BeanDefinitionRegistry beanDefinitionRegistry = (BeanDefinitionRegistry) factory;
+	beanDefinition.setAutowireCandidate(true);
+	beanDefinitionRegistry.registerBeanDefinition("xxx", beanDefinition);
+
+	CommandGroup group = Application.instance().getLifecycleAdvisor().getMenuBarCommandGroup();
+	CommandGroup ac = (CommandGroup) group.find("windowMenu/showViewMenu");
+
+	ViewDescriptor viewDescriptor = (ViewDescriptor) Application.instance()
+		.getApplicationContext().getBean("xxx");
+	ActionCommand actionCommand = viewDescriptor.createShowViewCommand(Application.instance()
+		.getActiveWindow());
+	actionCommand.setLabel("xx");
+
+	ac.add(actionCommand, true);
     }
 
 }
