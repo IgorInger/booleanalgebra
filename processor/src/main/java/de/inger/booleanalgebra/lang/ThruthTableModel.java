74a396d52589c67942028506dd56963b2bb469ca
Igor Inger <igor.inger@gmail.com>
2012-05-13 22:26:00 +0200

    Clean up

--- processor/src/main/java/de/inger/booleanalgebra/lang/ThruthTableModel.java
+++
@@ -1,7 +0,0 @@
-package de.inger.booleanalgebra.lang;
-
-import javax.swing.table.DefaultTableModel;
-
-public class ThruthTableModel extends DefaultTableModel {
-
-}
--- processor/src/main/java/de/inger/booleanalgebra/lang/ThruthTableModelFactory.java
+++ processor/src/main/java/de/inger/booleanalgebra/lang/ThruthTableModelFactory.java
@@ -1,25 +1,5 @@
 package de.inger.booleanalgebra.lang;
 
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Set;
-
-import de.inger.booleanalgebra.antlr3.treenodes.Operand;
-import de.inger.booleanalgebra.antlr3.treenodes.Operator;
-import de.inger.booleanalgebra.antlr3.treenodes.Variable;
-
 public class ThruthTableModelFactory {
-
-    public static ThruthTableModel buildThruthTableModel(Operator operator) {
-	return null;
-    }
-
-    public void countVariables(Operator operator) {
-	Set<Variable> variables = new HashSet<Variable>();
-
-    }
-
-    public void walk(Operand operand) {
-    }
 
 }
